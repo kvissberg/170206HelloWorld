013ab86cf58e6b65b32bd2808f8d893e01e0a586
kvissberg <max.kvissberg@gmail.com>
2017-02-06 16:19:00 +0100

    enKommentar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
+        //max gör en kommentar här för å se om de funkar...
+
         System.out.println("hello world");
     }
 }
