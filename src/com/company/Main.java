2b918f2a820775383583562b92eddf21c0d88a3a
SauronSauron <sauren.thomas@gmail.com>
2017-02-06 17:25:27 +0100

    svar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         //max gör en kommentar här för å se om de funkar...
+        // thomas hälsar att det funkar!
 
         System.out.println("hello world");
     }
